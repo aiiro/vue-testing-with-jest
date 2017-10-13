403496b8cace5bfee82a33cdab1d061fbfbc2c49
aiiro <aiiro@users.noreply.github.com>
2017-10-13 18:06:56 +0900

    Vue.js unit test with Jest.

---
+++ src/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{ message }}, {{ name }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        data() {
+            return {
+                message: 'Hello',
+            }
+        },
+        computed: {
+            greet() {
+                return this.message + ', ' + this.name;
+            }
+        },
+        methods: {
+            greetToUpper() {
+                return this.greet.toUpperCase();
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Hello from './Hello.vue'
+
+Vue.component('hello-item', Hello);
 
 new Vue({
   el: '#app',
